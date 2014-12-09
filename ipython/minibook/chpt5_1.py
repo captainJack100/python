62bac866c2d54e275985e7ea53b452eea375cb55
captainjack <captain@jack.com>
2014-12-09 10:22:48 -0800

    added code

--- ipython/minibook/chpt5_1.py
+++ ipython/minibook/chpt5_1.py
@@ -1,1 +1,22 @@
+""" Chapt5_1
+Shows some basics of running ipython's parallel library
+map_sync is parallel version of map.
+"""
+
 from IPython.parallel import Client
+
+def f(x):
+    return x*x
+
+def main():
+    rc = Client()
+    print rc.ids
+    
+    v = rc[:]
+    with v.sync_imports():
+        import time
+
+    print v.map_sync(f, range(10))
+
+if __name__ == "__main__":
+    main()
